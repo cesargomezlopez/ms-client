6d9357b1eceb914e9442237f1ed935c16c287f63
Cesar Gomez <cesar.gomez@urp.edu.pe>
2020-04-04 23:17:49 -0500

    Eureka Client Configuration

--- src/test/java/ms/client/MsClientApplicationTests.java
+++ src/test/java/ms/client/MsClientApplicationTests.java
@@ -2,7 +2,9 @@ package ms.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootTest
 class MsClientApplicationTests {
 
